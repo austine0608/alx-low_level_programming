38af08125cdc70579a5fd9aa6b9fd5c801026286
austine0608 <austinelakemfa@gmail.com>
2023-02-17 02:24:28 -0800

    New Commit7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print out sizes of data types in c
+ * main - print out sizes of data types in C
  * code by lakemfa tech
  * Return: 0
 */
@@ -17,5 +17,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
-	Return (0);
-
+	return (0);
+}
